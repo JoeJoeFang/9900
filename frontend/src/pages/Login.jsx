54ae1ef97e43ffb3c565825fe32c1b68405a4c29
WENYI MA <mawenyi1999@gmail.com>
2024-03-05 00:40:17 +1100

    updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -45,59 +45,6 @@ const LoginHost = () => {
         }
     };
 
-    // return (
-    //     <>
-    //         <Container maxWidth="xs">
-    //             <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
-    //                 <Typography component="h1" variant="h5">Login</Typography>
-    //                 <Box component="form" onSubmit={loginUser} noValidate sx={{ mt: 1 }}>
-    //                     <TextField
-    //                         margin="normal"
-    //                         required
-    //                         fullWidth
-    //                         id="email"
-    //                         label="Email Address"
-    //                         name="email"
-    //                         autoComplete="email"
-    //                         autoFocus
-    //                         value={loginData.email}
-    //                         onChange={updateField}
-    //                     />
-    //                     <TextField
-    //                         margin="normal"
-    //                         required
-    //                         fullWidth
-    //                         name="password"
-    //                         label="Password"
-    //                         type="password"
-    //                         id="password"
-    //                         autoComplete="current-password"
-    //                         value={loginData.password}
-    //                         onChange={updateField}
-    //                     />
-    //                     <Button
-    //                         type="submit"
-    //                         fullWidth
-    //                         variant="contained"
-    //                         sx={{ mt: 3, mb: 2 }}
-    //                     >
-    //                         Login
-    //                     </Button>
-    //                     {/* Add a button for navigating to the RegisterPage */}
-    //                     <Button
-    //                         fullWidth
-    //                         variant="text"
-    //                         sx={{ mt: 2 }}
-    //                         onClick={() => navigate('/register')}
-    //                     >
-    //                         Don&apos;t have an account? Register
-    //                     </Button>
-    //
-    //                 </Box>
-    //             </Box>
-    //         </Container>
-    //     </>
-    // );
     return (
         <Box sx={{
             minHeight: '100vh',
