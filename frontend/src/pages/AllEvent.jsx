afc255aa66747bb954371c47c15cb97de5d86b58
WENYI MA <mawenyi1999@gmail.com>
2024-03-13 21:18:47 +1100

    updated

--- frontend/src/pages/AllEvent.jsx
+++ frontend/src/pages/AllEvent.jsx
@@ -40,167 +40,75 @@ const EventsList = () => {
 
         fetchEvents();
     }, []);
-
     return (
         <>
-        <Box sx={{
-            minHeight: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            background: `url(${process.env.PUBLIC_URL}/default_background.jpg), linear-gradient(to right, #e66465, #9198e5)`,
-            backgroundSize: 'cover, cover',
-            backgroundPosition: 'center, center',
-            p: theme.spacing(2),
-        }}>
-            <Box sx={{ position: 'absolute', top: 10, right: 10, display: 'flex' }}>
-            <CreateNewEvent />
-            <MyEvents />
-            <HostProfile />
-            <Logout />
-        </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', mb: theme.spacing(4) }}>
-                <Box component="img" src={`${process.env.PUBLIC_URL}/LogoImage.jpg`} sx={{ width: 150, height: 'auto', mb: 2 }} />
-                <Typography variant="h3" color="white" sx={{ fontWeight: 'bold', textAlign: 'center' }}>
-                    Our Amazing Ticket Platform
-                </Typography>
+            <Box sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                background: `url(${process.env.PUBLIC_URL}/default_background.jpg), linear-gradient(to right, #e66465, #9198e5)`,
+                backgroundSize: 'cover, cover',
+                backgroundPosition: 'center, center',
+                p: theme.spacing(2),
+            }}>
+                <Box sx={{ position: 'absolute', top: 10, right: 10, display: 'flex' }}>
+                    <CreateNewEvent />
+                    <MyEvents />
+                    <HostProfile />
+                    <Logout />
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', mb: theme.spacing(4) }}>
+                    <Box component="img" src={`${process.env.PUBLIC_URL}/LogoImage.jpg`} sx={{ width: 150, height: 'auto', mb: 2 }} />
+                    <Typography variant="h3" color="white" sx={{ fontWeight: 'bold', textAlign: 'center' }}>
+                        Our Amazing Ticket Platform
+                    </Typography>
+                </Box>
+                {isLoading ? (
+                    <CircularProgress />
+                ) : error ? (
+                    <Typography color="error">{error}</Typography>
+                ) : events.length > 0 ? (
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px', width: '90%' }}>
+                        <Typography variant="h4" gutterBottom>Upcoming Events</Typography>
+                        {events.map((event, index) => (
+                            <Card key={index} sx={{ display: 'flex', mb: 2, width: '100%', background: 'rgba(255, 255, 255, 0.8)' }}>
+                                {event.thumbnail && (
+                                    <CardMedia
+                                        component="img"
+                                        sx={{ width: 240, objectFit: 'cover' }}
+                                        image={`${process.env.PUBLIC_URL}/cute_cat.jpeg`}
+                                        alt={event.title}
+                                    />
+                                )}
+                                <CardContent sx={{ flex: '1', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {event.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Organizer: {event.organizerName}<br />
+                                        Type: {event.eventType}<br />
+                                        Seats: {event.seatingCapacity}<br />
+                                        Duration: {event.duration} hours<br />
+                                        From: {new Date(event.startDate).toLocaleDateString()}<br />
+                                        To: {new Date(event.endDate).toLocaleDateString()}<br />
+                                        Address: {event.address}<br />
+                                        Price: ${parseFloat(event.price).toFixed(2)}<br />
+                                        Description: {event.description.substring(0, 100)}{event.description.length > 100 ? '...' : ''}<br />
+                                        {event.youtubeUrl && <a href={event.youtubeUrl}>Event Video</a>}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </div>
+                ) : (
+                    <Typography variant="subtitle1">No events found.</Typography>
+                )}
             </Box>
-            {isLoading ? (
-                <CircularProgress />
-            ) : error ? (
-                <Typography color="error">{error}</Typography>
-            ) : events.length > 0 ? (
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
-                    <Typography variant="h4" gutterBottom>Upcoming Events</Typography>
-                    {events.map((event, index) => (
-                        <Card key={index} sx={{ maxWidth: 345, mb: 2, width: '100%' }}>
-                            {event.thumbnail && (
-                                <CardMedia
-                                    component="img"
-                                    height="140"
-                                    image={event.thumbnail}
-                                    alt={event.title}
-                                />
-                            )}
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {event.title}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Organizer: {event.organizerName}<br />
-                                    Type: {event.eventType}<br />
-                                    Seats: {event.seatingCapacity}<br />
-                                    Duration: {event.duration} hours<br />
-                                    From: {new Date(event.startDate).toLocaleDateString()}<br />
-                                    To: {new Date(event.endDate).toLocaleDateString()}<br />
-                                    Address: {event.address}<br />
-                                    Price: ${parseFloat(event.price).toFixed(2)}<br />
-                                    Description: {event.description}<br />
-                                    {event.youtubeUrl && <a href={event.youtubeUrl}>Event Video</a>}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))}
-                </div>
-            ) : (
-                <Typography variant="subtitle1">No events found.</Typography>
-            )}
-        </Box>
-        </> 
+        </>
     );
+
 };
 
 export default EventsList;
-
-// const EventsList = () => {
-//     const [events, setEvents] = useState([]);
-//     const [isLoading, setIsLoading] = useState(false);
-//     const [error, setError] = useState(null);
-//     console.log("landing all event page");
-//
-//     useEffect(() => {
-//         const fetchEvents = async () => {
-//             setIsLoading(true);
-//             setError(null);
-//             try {
-//                 const response = await axios.get('http://localhost:5000/events');
-//                 setEvents(response.data);
-//             } catch (error) {
-//                 console.error("There was an error fetching the events:", error);
-//                 setError("Failed to load events. Please try again later.");
-//             } finally {
-//                 setIsLoading(false);
-//             }
-//         };
-//
-//         fetchEvents();
-//     }, []);
-//
-//     if (isLoading) {
-//         return <CircularProgress />;
-//     }
-//
-//     if (error) {
-//         return <Typography color="error">{error}</Typography>;
-//     }
-//
-//     return (
-//         <Box sx={{
-//             minHeight: '100vh',
-//             display: 'flex',
-//             flexDirection: 'column',
-//             justifyContent: 'center',
-//             alignItems: 'center',
-//             background: `url(${process.env.PUBLIC_URL}/default_background.jpg), linear-gradient(to right, #e66465, #9198e5)`,
-//             backgroundSize: 'cover, cover',
-//             backgroundPosition: 'center, center',
-//             p: theme.spacing(2),
-//         }}>
-//             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', mb: theme.spacing(4) }}>
-//                 <Box component="img" src={`${process.env.PUBLIC_URL}/LogoImage.jpg`} sx={{ width: 150, height: 'auto', mb: 2 }} />
-//                 <Typography variant="h3" color="white" sx={{ fontWeight: 'bold', textAlign: 'center' }}>
-//                     Our Amazing Ticket Platform
-//                 </Typography>
-//             </Box>
-//             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
-//                 <Typography variant="h4" gutterBottom>Upcoming Events</Typography>
-//                 {events.length > 0 ? (
-//                     events.map((event, index) => (
-//                         <Card key={index} sx={{ maxWidth: 345, mb: 2, width: '100%' }}>
-//                             {event.thumbnail && (
-//                                 <CardMedia
-//                                     component="img"
-//                                     height="140"
-//                                     image={event.thumbnail}
-//                                     alt={event.title}
-//                                 />
-//                             )}
-//                             <CardContent>
-//                                 <Typography gutterBottom variant="h5" component="div">
-//                                     {event.title}
-//                                 </Typography>
-//                                 <Typography variant="body2" color="text.secondary">
-//                                     Organizer: {event.organizerName}<br />
-//                                     Type: {event.eventType}<br />
-//                                     Seats: {event.seatingCapacity}<br />
-//                                     Duration: {event.duration} hours<br />
-//                                     From: {new Date(event.startDate).toLocaleDateString()}<br />
-//                                     To: {new Date(event.endDate).toLocaleDateString()}<br />
-//                                     Address: {event.address}<br />
-//                                     Price: ${parseFloat(event.price).toFixed(2)}<br />
-//                                     Description: {event.description}<br />
-//                                     {event.youtubeUrl && <a href={event.youtubeUrl}>Event Video</a>}
-//                                 </Typography>
-//                             </CardContent>
-//                         </Card>
-//                     ))
-//                 ) : (
-//                     <Typography variant="subtitle1">No events found.</Typography>
-//                 )}
-//             </div>
-//         </Box>
-//     );
-// };
-//
-// export default EventsList;
